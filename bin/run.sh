3e827fd36ebabccbc456389ece3fca841f8f4472
David Eriksson <david.eriksson@softhouse.se>
2012-04-26 16:07:30 +0200

    Template

---
+++ bin/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ndb_mgm --try-reconnect=1 -e show 2>&1 >/dev/null ; then
+	echo "ndb_mgd is already running" >&2
+	exit 1
+fi
+
+cd $HOME/my_cluster
+ndb_mgmd -f conf/config.ini --initial --configdir=$HOME/my_cluster/conf/
+sleep 1
+ndbd -c localhost:1186
+ndbd -c localhost:1186
+while ndb_mgm -e show | tee /tmp/show | egrep -q '^id=(3|4)[[:space:]].*(not connected|starting)'; do 
+	echo -n '.'
+	sleep 1
+done
+echo ''
+echo "Starting mysqld"
+nohup mysqld --defaults-file=conf/my.cnf &
+
---
+++ bin/shutdown.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mysqladmin -u root -h 127.0.0.1 -P 5000 shutdown
+ndb_mgm --try-reconnect=1 -e shutdown
+
