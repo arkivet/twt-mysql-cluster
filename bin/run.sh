c537dcb80596631ab0bb96cd2ea2e01f9213bf5f
David Eriksson <david.eriksson@softhouse.se>
2012-04-26 16:24:28 +0200

    More path fixes

--- bin/run.sh
+++ bin/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 PATH=$PATH:/opt/mysql/server-5.5/bin
+cd `dirname $0`/..
+MY_CLUSTER=`pwd`/my_cluster
 
 if ndb_mgm --try-reconnect=1 -e show 2>&1 >/dev/null ; then
 	echo "ndb_mgd is already running" >&2
 	exit 1
 fi
 
-cd $HOME/my_cluster
-ndb_mgmd -f conf/config.ini --initial --configdir=$HOME/my_cluster/conf/
+cd $MY_CLUSTER
+ndb_mgmd -f conf/config.ini --initial --configdir=$MY_CLUSTER/conf/
 sleep 1
 ndbd -c localhost:1186
 ndbd -c localhost:1186
